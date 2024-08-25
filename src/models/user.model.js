2500e6275bf7aa1c76359a9825bbfe21b413b066
Anubhav <anubhavsarangi517@gmail.com>
2024-08-25 20:16:09 +0530

    add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -29,7 +29,7 @@ const userSchema=new Schema(
                 required:true,
             },
             coverImage:{
-                tyep:String,//cloudinary URL
+                type:String,//cloudinary URL
             },
             watchHistory:[
                 {
@@ -52,7 +52,7 @@ const userSchema=new Schema(
 
 userSchema.pre("save",async function(next) {
     if(!this.isModified("password")) return next();
-    this.password=bcrypt.hash(this.password,10)
+    this.password=await bcrypt.hash(this.password,10)
     next()
 })
 
